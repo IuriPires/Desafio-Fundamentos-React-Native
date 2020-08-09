69b98dc63b375d4873476fc81266f187ad070f04
Iuri Pires <iuripires.work@gmail.com>
2020-08-09 14:37:58 -0300

    removing unnecessary console.log

--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -31,7 +31,6 @@ const CartProvider: React.FC = ({ children }) => {
   useEffect(() => {
     async function loadProducts(): Promise<void> {
       // TODO LOAD ITEMS FROM ASYNC STORAGE
-      console.log('test');
     }
 
     loadProducts();
