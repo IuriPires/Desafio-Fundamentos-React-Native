4f31288148f6c1814764484d5cef678102f4692a
Iuri Pires <iuripires.work@gmail.com>
2020-08-09 14:37:25 -0300

    .

--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -31,6 +31,7 @@ const CartProvider: React.FC = ({ children }) => {
   useEffect(() => {
     async function loadProducts(): Promise<void> {
       // TODO LOAD ITEMS FROM ASYNC STORAGE
+      console.log('test');
     }
 
     loadProducts();
